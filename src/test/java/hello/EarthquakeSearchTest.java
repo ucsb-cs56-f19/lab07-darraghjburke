c02de154dbaa075c7f52e9b525c38fe8b0c5c19e
Darragh Burke <darragh@ucsb.edu>
2019-11-14 19:54:16 -0800

    db add mocking to earthquake

--- src/test/java/hello/EarthquakeSearchTest.java
+++ src/test/java/hello/EarthquakeSearchTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(WebController.class)
 @AutoConfigureMockMvc
 public class EarthquakeSearchTest {
 
