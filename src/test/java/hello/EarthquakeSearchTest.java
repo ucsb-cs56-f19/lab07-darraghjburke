aaa561c1605cb3991b83838f1a21346710c72838
Darragh Burke <darragh@ucsb.edu>
2019-11-14 19:59:55 -0800

    db remove mvc bean

--- src/test/java/hello/EarthquakeSearchTest.java
+++ src/test/java/hello/EarthquakeSearchTest.java
@@ -1,26 +1,16 @@
 package hello;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(WebController.class)
 @AutoConfigureMockMvc
 public class EarthquakeSearchTest {
-
-    @Autowired
-    private MockMvc mvc;
 
     @Test
     public void getEarthquakeSearch() throws Exception {
